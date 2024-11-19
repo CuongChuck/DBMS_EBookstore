fd1908662de9b22a27ca69f3d27a574bdb1f7cd8
CuongChuck <englishtalent123@gmail.com>
2024-11-19 23:59:03 +0700

    Update Routes for Publisher & Author

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,17 +1,27 @@
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
+import Admin from './pages/Home/Admin';
+import User from './pages/Home/User';
 import AddPublisher from './pages/Publisher/AddPublisher';
 import ShowPublisherList from './pages/Publisher/ShowPublisherList';
-import ShowPublisher from './pages/Publisher/ShowPublisher';
 import EditPublisher from './pages/Publisher/EditPublisher';
 import DeletePublisher from './pages/Publisher/DeletePublisher';
+import ShowAuthorList from './pages/Author/ShowAuthorList';
+import AddAuthor from './pages/Author/AddAuthor';
+import DeleteAuthor from './pages/Author/DeleteAuthor';
+import EditAuthor from './pages/Author/EditAuthor';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/admin', component: Admin },
+    { path: '/user', component: User },
     { path: '/publisher/admin/add', component: AddPublisher },
     { path: '/publisher/list', component: ShowPublisherList },
-    { path: '/publisher/details/:name', component: ShowPublisher },
     { path: '/publisher/admin/edit/:id', component: EditPublisher },
-    { path: '/publisher/admin/delete/:name', component: DeletePublisher },
+    { path: '/publisher/admin/delete/:id', component: DeletePublisher },
+    { path: '/author/list', component: ShowAuthorList },
+    { path: '/author/admin/add', component: AddAuthor },
+    { path: '/author/admin/delete/:id', component: DeleteAuthor },
+    { path: '/author/admin/edit/:id', component: EditAuthor }
 ];
 
 export { routes };
