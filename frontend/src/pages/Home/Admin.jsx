b5bf36c236d2fe0727db26cb94f20f7bdb570261
CuongChuck <englishtalent123@gmail.com>
2024-11-21 00:08:33 +0700

    Update frontend for Home pages

--- frontend/src/pages/Home/Admin.jsx
+++ frontend/src/pages/Home/Admin.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Admin = () => {
+    const [auth, setAuth] = useState(false);
+    const navigate = useNavigate();
+    useEffect(() => {
+        axios
+            .get('http://localhost:8080/')
+            .then((response) => {
+                if (response.data.status === "Success") setAuth(true);
+                else setAuth(false);
+            })
+
+    }, []);
+
+    const handleLogOut = () => {
+        axios
+            .get('http://localhost:8080/logout')
+            .then((response) => {
+                if (response.data.message === "Success") navigate('/');
+                else alert("Error occurred!");
+            })
+            .catch((err) => {
+                console.error(err.message);
+            })
+    };
+
     return (
         <div>
-            <h2>Dashboard</h2>
-            <ul>
-                <li><Link to={'/admin'} >Home</Link></li>
-                <li><Link to={'/publisher/list'}>Publisher</Link></li>
-                <li><Link to={'/author/list'}>Author</Link></li>
-                <li><Link to={'/translator/list'}>Translator</Link></li>
-                <li><Link to={'/category/list'}>Category</Link></li>
-            </ul>
+            {
+                auth ?
+                <div>
+                    <h2>Dashboard</h2>
+                    <ul>
+                        <li><Link to={'/admin'} >Home</Link></li>
+                        <li><Link to={'/publisher/list'}>Publisher</Link></li>
+                        <li><Link to={'/author/list'}>Author</Link></li>
+                        <li><Link to={'/translator/list'}>Translator</Link></li>
+                        <li><Link to={'/category/list'}>Category</Link></li>
+                    </ul>
+                    <button onClick={handleLogOut}>Logout</button>
+                </div>
+                :
+                <div>
+                    <h2>Error. Log in again</h2>
+                    <button onClick={() => {navigate('/admin/auth')}}>Log In</button>
+                </div>
+            }
         </div>
     )
 }
--- frontend/src/pages/Home/AdminAuth.jsx
+++ frontend/src/pages/Home/AdminAuth.jsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const AdminAuth = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    axios.defaults.withCredentials = true;
+    const handleLogIn = () => {
+        const data = {username, password};
+        axios
+            .post('http://localhost:8080/login', data)
+            .then((response) => {
+                if (response.data.status === "Success") navigate('/admin');
+                else alert("Username or password is not correct");
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    };
+
     return (
         <div>
             <div>
                 <h2>Log In</h2>
                 <div>
                     <label>Username</label>
-                    <input type='text' />
+                    <input
+                        type='text'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type='password' />
+                    <input
+                        type='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
-                    <button>Log In</button>
+                    <button onClick={handleLogIn}>Log In</button>
                 </div>
             </div>
         </div>
