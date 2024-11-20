f8a53d2743b6950eb10c2bb1c7ea4afbc31d9772
CuongChuck <englishtalent123@gmail.com>
2024-11-21 00:08:52 +0700

    Update backend for Authentication

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, mysqlConnection } from "./config.js";
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import publisherRouter from "./routes/publisherRouter.js";
 import authorRouter from "./routes/authorRouter.js";
 import authRouter from "./routes/authRouter.js";
@@ -11,19 +12,15 @@ const app = express();
 
 // Parse JSON requests
 app.use(express.json());
+app.use(cookieParser());
 
 // Handle CORS policy
-app.use(cors());
-// app.use(cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"]
-// }));
-
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(200).send("Hello World!");
-});
+// app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.listen(PORT, () => {
     console.log(`App is listening to port ${PORT}`);
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,7 +1,30 @@
 import express from 'express';
 import { mysqlConnection } from '../config.js';
+import jwt from 'jsonwebtoken';
 
 const authRouter = express.Router();
+
+const verifyAdmin = (request, response, next) => {
+    const token = request.cookies.token;
+    if (!token) return response.json({message: "Token needed"});
+    else {
+        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+            if (err) return response.json({message: "Authentication Error."});
+            else next();
+        });
+    }
+}
+
+// Route for confirm log in
+authRouter.get('/', verifyAdmin, (request, response) => {
+    return response.json({status: "Success"});
+});
+
+// Route for logout
+authRouter.get('/logout', (request, response) => {
+    response.clearCookie('token');
+    return response.json({message: "Success"});
+});
 
 // Route for Register
 authRouter.post('/register', async (request, response) => {
@@ -29,13 +52,12 @@ authRouter.post('/register', async (request, response) => {
     }
 });
 
-// Route for Admin Log In
-authRouter.get('/', async (request, response) => {
+// Route for User list
+authRouter.get('/user-list', async (request, response) => {
     try {
-        const sql = `SELECT * FROM Publisher`;
+        const sql = `SELECT * FROM User`;
         mysqlConnection.query(sql, (err, results, fields) => {
-            if (err) return console.error(err.message);
-            console.log(results);
+            if (err) return response.status(404).send({message: err.message});
             return response.status(200).json({
                 data: results
             });
@@ -46,20 +68,28 @@ authRouter.get('/', async (request, response) => {
     }
 });
 
-// Route for User Log In
-authRouter.get('/', async (request, response) => {
+// Route for Log In
+authRouter.post('/login', async (request, response) => {
     try {
-        const sql = `SELECT * FROM Publisher`;
-        mysqlConnection.query(sql, (err, results, fields) => {
-            if (err) return console.error(err.message);
-            console.log(results);
-            return response.status(200).json({
-                data: results
+        if (!request.body.username || !request.body.password) {
+            return response.status(400).send({
+                message: "Send all required fields: username, password"
             });
+        }
+        const sql = `SELECT * FROM User WHERE Username = ?`;
+        mysqlConnection.query(sql, [request.body.username], (err, results, fields) => {
+            if (err || results.length == 0 || request.body.password != results[0].Password)
+                return response.status(404).json({status: "Failed"});
+            else {
+                const id = results[0].UserID;
+                const token = jwt.sign({id}, "jwt-secret-key", {expiresIn: '1d'});
+                response.cookie('token', token);
+                return response.status(200).json({status: "Success"});
+            }
         });
     } catch (err) {
         console.error(err.message);
-        response.status(500).send({message: err.message});
+        response.status(500).json({status: "Failed"});
     }
 });
 
