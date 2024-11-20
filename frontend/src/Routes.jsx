30173cd7b2af1836e4397f93228480e09043c575
CuongChuck <englishtalent123@gmail.com>
2024-11-20 17:19:53 +0700

    Update frontend

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,4 +1,6 @@
 import Home from './pages/Home/Home';
+import Register from './pages/Home/Register';
+import AdminAuth from './pages/Home/AdminAuth';
 import Admin from './pages/Home/Admin';
 import User from './pages/Home/User';
 import AddPublisher from './pages/Publisher/AddPublisher';
@@ -9,10 +11,16 @@ import ShowAuthorList from './pages/Author/ShowAuthorList';
 import AddAuthor from './pages/Author/AddAuthor';
 import DeleteAuthor from './pages/Author/DeleteAuthor';
 import EditAuthor from './pages/Author/EditAuthor';
+import ShowTranslatorList from './pages/Translator/ShowTranslatorList';
+import AddTranslator from './pages/Translator/AddTranslator';
+import DeleteTranslator from './pages/Translator/DeleteTranslator';
+import EditTranslator from './pages/Translator/EditTranslator';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/register', component: Register },
     { path: '/admin', component: Admin },
+    { path: '/admin/auth', component: AdminAuth },
     { path: '/user', component: User },
     { path: '/publisher/admin/add', component: AddPublisher },
     { path: '/publisher/list', component: ShowPublisherList },
@@ -21,7 +29,11 @@ const routes = [
     { path: '/author/list', component: ShowAuthorList },
     { path: '/author/admin/add', component: AddAuthor },
     { path: '/author/admin/delete/:id', component: DeleteAuthor },
-    { path: '/author/admin/edit/:id', component: EditAuthor }
+    { path: '/author/admin/edit/:id', component: EditAuthor },
+    { path: '/translator/list', component: ShowTranslatorList },
+    { path: '/translator/admin/add', component: AddTranslator },
+    { path: '/translator/admin/delete/:id', component: DeleteTranslator },
+    { path: '/translator/admin/edit/:id', component: EditTranslator }
 ];
 
 export { routes };
