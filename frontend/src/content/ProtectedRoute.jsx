f74fcd8ec9023eb0c40e9cafd4192f2d6637e7aa
CuongChuck <englishtalent123@gmail.com>
2024-11-28 22:04:55 +0700

    Add ProtectedRoute to distinguish user and admin

---
+++ frontend/src/content/ProtectedRoute.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({children, requiredRole}) => {
+    const [adminAuthorized, setAdminAuthorized] = useState(null);
+    const [userAuthorized, setUserAuthorized] = useState(null);
+    useEffect(() => {
+        const authenticate = async () => {
+            try {
+                const [adminResponse, userResponse] = await Promise.all([
+                    axios.get('http://localhost:8080/admin/auth'),
+                    axios.get('http://localhost:8080/user/auth')
+                ]);
+                if (adminResponse.data.status === "Admin Authorized" && requiredRole == "admin")
+                    setAdminAuthorized(true);
+                else setAdminAuthorized(false);
+                if (userResponse.data.status === "User Authorized" && requiredRole == "user") {
+                    setUserAuthorized(true);
+                }
+                else setUserAuthorized(false);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        authenticate();
+    }, [requiredRole]);
+    
+    if (adminAuthorized || userAuthorized) return children;
+}
+
+export default ProtectedRoute
