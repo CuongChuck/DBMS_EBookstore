c986e8ff7e332d52e62e2c2b2811af22b636fcad
CuongChuck <englishtalent123@gmail.com>
2024-11-28 22:04:27 +0700

    Add navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,45 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { routes } from './Routes'
+import ProtectedRoute from './content/ProtectedRoute'
 
 const App = () => {
     return (
-        <Routes>
-            {routes.map((route, index) => {
-                const Page = route.component;
-                return (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        element={<Page />}
-                    />
-                );
-            })}
-        </Routes>
+        <div>
+            <ul>
+                <li><Link to={'/user'} >Home</Link></li>
+                <li><Link to={'/publisher'}>Publisher</Link></li>
+                <li><Link to={'/author'}>Author</Link></li>
+                <li><Link to={'/translator'}>Translator</Link></li>
+                <li><Link to={'/category'}>Category</Link></li>
+                <li><Link to={'/book'}>Book</Link></li>
+            </ul>
+            <Routes>
+                {routes.map((route, index) => {
+                    const Page = route.component;
+                    if (index > 12) {
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={<Page />}
+                            />
+                        );
+                    }
+                    else return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <ProtectedRoute requiredRole={"admin"}>
+                                    <Page />
+                                </ProtectedRoute>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </div>
     )
 }
 
