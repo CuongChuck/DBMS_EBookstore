d6be6457779fc728dc35ed684bd23b2855947962
CuongChuck <englishtalent123@gmail.com>
2024-11-28 22:04:16 +0700

    Update routes, join user and admin

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,7 +1,5 @@
 import Home from './pages/Home/Home';
 import Register from './pages/Home/Register';
-import AdminAuth from './pages/Home/AdminAuth';
-import Admin from './pages/Home/Admin';
 import UserAuth from './pages/Home/UserAuth';
 import User from './pages/Home/User';
 import AddPublisher from './pages/Publisher/AddPublisher';
@@ -20,30 +18,36 @@ import ShowCategoryList from './pages/Category/ShowCategoryList';
 import AddCategory from './pages/Category/AddCategory';
 import DeleteCategory from './pages/Category/DeleteCategory';
 import EditCategory from './pages/Category/EditCategory';
+import ShowBookList from './pages/Book/ShowBookList';
+import AddBook from './pages/Book/AddBook';
+import ShowAuthor from './pages/Author/ShowAuthor';
+import ShowBook from './pages/Book/ShowBook';
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/register', component: Register },
-    { path: '/admin', component: Admin },
-    { path: '/user', component: User },
-    { path: '/admin/auth', component: AdminAuth },
-    { path: '/user/auth', component: UserAuth },
     { path: '/publisher/add', component: AddPublisher },
-    { path: '/publisher/list', component: ShowPublisherList },
     { path: '/publisher/edit/:id', component: EditPublisher },
     { path: '/publisher/delete/:id', component: DeletePublisher },
-    { path: '/author/list', component: ShowAuthorList },
     { path: '/author/add', component: AddAuthor },
     { path: '/author/delete/:id', component: DeleteAuthor },
     { path: '/author/edit/:id', component: EditAuthor },
-    { path: '/translator/list', component: ShowTranslatorList },
     { path: '/translator/add', component: AddTranslator },
     { path: '/translator/delete/:id', component: DeleteTranslator },
     { path: '/translator/edit/:id', component: EditTranslator },
-    { path: '/category/list', component: ShowCategoryList },
     { path: '/category/add', component: AddCategory },
     { path: '/category/delete/:id', component: DeleteCategory },
-    { path: '/category/edit/:id', component: EditCategory }
+    { path: '/category/edit/:id', component: EditCategory },
+    { path: '/book/add', component: AddBook },
+    { path: '/user', component: User },
+    { path: '/', component: Home },
+    { path: '/register', component: Register },
+    { path: '/user/auth', component: UserAuth },
+    { path: '/book', component: ShowBookList },
+    { path: '/book/:id', component: ShowBook },
+    { path: '/author/:id', component: ShowAuthor },
+    { path: '/author', component: ShowAuthorList },
+    { path: '/publisher', component: ShowPublisherList },
+    { path: '/translator', component: ShowTranslatorList },
+    { path: '/category', component: ShowCategoryList }
 ];
 
 export { routes };
