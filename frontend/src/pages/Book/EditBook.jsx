f64a483c94e95172b54ea4503eee5f348126d037
CuongChuck <87120150+CuongChuck@users.noreply.github.com>
2024-12-09 07:54:02 +0700

    Update EditBook.jsx

--- frontend/src/pages/Book/EditBook.jsx
+++ frontend/src/pages/Book/EditBook.jsx
@@ -3,19 +3,41 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const EditBook = () => {
-    const [name, setName] = useState('');
+    const [name, setName] = useState(null);
     const [oriPrice, setOriPrice] = useState(0);
-    const [date, setDate] = useState('');
+    const [date, setDate] = useState(null);
     const [quantityStored, setQuantityStored] = useState(0);
-    const [quantitySold, setQuantitySold] = useState(0);
-    const [description, setDescription] = useState('');
-    const [sellPrice, setSellPrice] = useState(0);
+    const [quantitySold, setQuantitySold] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [sellPrice, setSellPrice] = useState(null);
     const [publisher, setPublisher] = useState(null);
     const navigate = useNavigate();
     const { id } = useParams();
 
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/book/${id}`)
+            .then((response) => {
+                setName(response.data.data[0][0].BookName);
+                setDescription(response.data.data[0][0].Description);
+                setSellPrice(response.data.data[0][0].SellingPrice);
+                setPublisher(response.data.data[0][0].PublisherID);
+                setOriPrice(response.data.data[0][0].OriginalPrice);
+                setDate(response.data.data[0][0].PublicationDate);
+                setQuantityStored(response.data.data[0][0].QuantityStored);
+                setQuantitySold(response.data.data[0][0].QuantitySold);
+            })
+            .catch((err) => {
+                console.error(err);
+                alert("An error occurred when getting info of a book");
+            })
+    }, [id])
+
     const handleEditBook = () => {
-        const data = { name, description };
+        const data = {
+            name, oriPrice, quantityStored, publisher, date,
+            quantitySold, description, sellPrice
+        };
         axios
             .put(`http://localhost:8080/book/edit/${id}`, data)
             .then(() => {
@@ -23,7 +45,7 @@ const EditBook = () => {
             })
             .catch((err) => {
                 console.error(err);
-                alert("An error occurred when editing an book");
+                alert("An error occurred when editing a book");
             })
     };
 
